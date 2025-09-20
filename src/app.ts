c69117c7c79b1c0ad5af1633c8b18acb91f2bc66
Yuri <yuque7@gmail.com>
2025-09-20 10:35:43 -0300

    Adicionando ao app.ts as rotas necessárias

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from 'express'
 import cors from 'cors'
+import { userRouter } from './routes/userRouter'
 
 export const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use('/users', userRouter)
+app.use('/posts', userRouter)
+
 
 app.listen(3003, () => {
     console.log('Servidor rodando na porta 3003')
